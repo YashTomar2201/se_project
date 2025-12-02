19b7535f808aa1e61493bbe807a6c277faa6cc9e
Yash Tomar <yashtomar2201@gmail.com>
2025-12-03 04:08:47 +0530

    url_locator_update_2

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const URL = window.location.href.includes('localhost')
   ? 'http://localhost:5000/api'
-  : 'https://se-project-nine-self.vercel.app/api';
+  : 'https://se-project-1jrr.vercel.app/api';
