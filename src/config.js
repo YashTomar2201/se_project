b7cf33f43134ad289a8d195790fd2796ba885068
Yash Tomar <yashtomar2201@gmail.com>
2025-12-03 05:50:52 +0530

    new link

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// Check if we are running locally on our computer
 export const URL = window.location.href.includes('localhost')
-  ? 'http://localhost:5000/api'
-  : 'https://se-project-1jrr.vercel.app/api';
+  ? 'http://localhost:5000/api'  // Local Backend
+  : 'https://se-project-gkzu.onrender.com/api'; // <--- YOUR NEW RENDER BACKEND
